db04afd664dd2a9bd990cbb4805f1d523a20965b
HirudineeADRT <hirudinee@adroitlogic.com>
2020-01-22 14:44:12 +0530

    tokyo

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.smoketokyo2201function = require("./smoketokyo2201/function.js").handler;
